be895ee62292146ec391ec893f1cb819474672fe
Huguesitel <68650794+Huguesitel@users.noreply.github.com>
2020-10-05 14:55:11 +0200

    Update Test2.js

--- BB/Test2.js
+++ BB/Test2.js
@@ -1,20 +1,3 @@
-import java.io.File;  // Import the File class
-import java.io.IOException;  // Import the IOException class to handle errors
-
-public class CreateFile {
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("Data/filename.txt");
-      if (myObj.createNewFile()) {
-        System.out.println("File created: " + myObj.getName());
-        alert("File created: " + myObj.getName())
-      } else {
-        System.out.println("File already exists.");
-      }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      alert("An error occurred.")
-      e.printStackTrace();
-    }
-  }
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
