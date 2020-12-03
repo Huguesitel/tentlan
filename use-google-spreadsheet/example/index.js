c6b337491705160ab4eaf2f6a26976b4488e81c9
Huguesitel <68650794+Huguesitel@users.noreply.github.com>
2020-12-03 19:25:18 +0100

    Update index.js

--- use-google-spreadsheet/example/index.js
+++ use-google-spreadsheet/example/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import useGoogleSpreadsheet from '../lib';
 
 const urls = [
-	'https://docs.google.com/spreadsheets/d/1W5D9WvlrXvndEc0b42OsdzJTT1M-MxKVYdPEtleqRQY/edit?usp=sharing',
+	'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM2HlAsG7qP7tL63eD47o_0_iJFHJSCjkZVT1StBDIKvCn1X0Nf1BBu88W7ZU26pjMhUL91DfexWV2/pubhtml',
 	'https://docs.google.com/spreadsheets/d/1XqhOiYajpp7zzj3ncJ8EpCKZoNOJSjiQdLTXCiBqCWc/edit?usp=sharing',
 ];
 
-const API_KEY = 'AIzaSyCdQT2rJxKIzwikcg9accLGBhQhaxkhSPw';
+const API_KEY = 'AIzaSyAGqRS1wyYGUBs5ZWI2GbqbcNteRqvKWjk';
 
 const Example = ({}) => {
 	const [index, setIndex] = useState(0);
